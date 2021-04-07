5c064dfc63dc8c1173a0d6a1c7b9a698de813a94
Ivan Cordova Rodriguez <talivan1602@gmail.com>
2021-04-07 16:27:10 -0500

    Agrera post

--- Usuarios/Usuarios.php
+++ Usuarios/Usuarios.php
@@ -8,5 +8,10 @@
 </head>
 <body>
     <h1>hola</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque vel fugiat dolorem iure, quasi doloremque harum suscipit fugit, doloribus animi, aspernatur expedita soluta atque explicabo est tempore? Vero, architecto fuga.</p>
+
+    <form action="#" method="get">
+        <button>Hola</button>
+    </form>
 </body>
 </html>
